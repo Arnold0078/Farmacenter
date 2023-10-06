013c2cb9b72988a24ac58b2544be7d017538c09d
Arnold Alvarez <arnoldhernandoalvarez@gmail.com>
2023-10-05 21:15:26 -0500

    Eliminamos modeloventana y solo se dejo ventana principal

--- src/co/edu/uptc/gui/ventanas/VentanaPrincipal.java
+++ src/co/edu/uptc/gui/ventanas/VentanaPrincipal.java
@@ -1,10 +1,16 @@
 package co.edu.uptc.gui.ventanas;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class VentanaPrincipal extends ModeloVentana{
+public class VentanaPrincipal extends JFrame{
 
     public VentanaPrincipal(){
+        setIconImage(Toolkit.getDefaultToolkit().getImage("imagenes/logo.png"));
+        setLayout(new BorderLayout());
+        setSize(600,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Farmacenter");
     }
 
--- src/co/edu/uptc/persistencia/Producto.java
+++ src/co/edu/uptc/persistencia/Producto.java
@@ -1,4 +1,51 @@
 package co.edu.uptc.persistencia;
 
 public class Producto {
+
+    private String nombre;
+    private String marca;
+    private int cantidad;
+    private int codigo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", marca='" + marca + '\'' +
+                ", cantidad=" + cantidad +
+                ", codigo=" + codigo +
+                '}';
+    }
 }
